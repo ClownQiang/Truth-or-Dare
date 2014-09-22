cc7d44ec2b620840f732a6ae2dd7866e4b0b88c5
1019439568 <www.dai1019439568@gmail.com>
2014-09-22 21:59:21 +0800

    UmengService基本添加好

--- RealWordAndBigAdventure_Beta/src/com/example/RealWordAndBigAdventure_Beta/MyActivity.java
+++ RealWordAndBigAdventure_Beta/src/com/example/RealWordAndBigAdventure_Beta/MyActivity.java
@@ -37,8 +37,6 @@ public class MyActivity extends Activity {
     private static float X_b=0,X_e=-0.05f,Y_b=0,Y_e=0.04f;
     //是否在旋转
     private static boolean pic_isRotating = false;
-    //private static boolean reset_IsClicking = false;
-    //private static boolean shadow_isRotating = false;
     //角度
     private static int angle = 0,randomangle=0;
 
@@ -52,7 +50,6 @@ public class MyActivity extends Activity {
         //Umeng自动更新
         UmengUpdateAgent.update(this);
 
-        //reset_bt = (Button)findViewById(R.id.reset);
         realword_bt = (Button)findViewById(R.id.realword_bt);
         bigadventure_bt = (Button)findViewById(R.id.bigadventure_bt);
         bottle = (ImageView)findViewById(R.id.bottle);
@@ -82,10 +79,6 @@ public class MyActivity extends Activity {
                             Animation.RELATIVE_TO_SELF, 0.35f);
                     //shadow图片旋转
                     AnimationSet animationSet_s = new AnimationSet(true);
-                    //阴影变化
-                    /*TranslateAnimation translateAnimation_b = new TranslateAnimation(
-                            Animation.RELATIVE_TO_SELF,X_b,Animation.RELATIVE_TO_SELF,X_e,
-                            Animation.RELATIVE_TO_SELF,Y_b,Animation.RELATIVE_TO_SELF,Y_e);*/
                     RotateAnimation rotateAnimation_s = new RotateAnimation(angle,randomangle,
                             Animation.RELATIVE_TO_SELF, 0.5f,
                             Animation.RELATIVE_TO_SELF, 0.35f);
@@ -93,56 +86,18 @@ public class MyActivity extends Activity {
                     //保证下一次在旋转前一次位置旋转
                     animationSet.setFillAfter(true);
                     animationSet_s.setFillAfter(true);
-                    //加速度设置为匀速
-                    //LinearInterpolator linearInterpolator = new LinearInterpolator();
 
                     angle = randomangle%360;
                     rotateAnimation.setDuration(randomangle+1000);
                     rotateAnimation_s.setDuration(randomangle+1000);
-                    /*rotateAnimation.setInterpolator(linearInterpolator);
-                    rotateAnimation_s.setInterpolator(linearInterpolator);*/
-                    //translateAnimation_b.setDuration(180*3);
-                    //translateAnimation_b.setRepeatCount(randomangle/180-3);
                     Log.d("Clownxiaoqiang","repeatCount:"+randomangle/180);
-                    //translateAnimation_b.setRepeatMode(Animation.REVERSE);
 
                     animationSet.addAnimation(rotateAnimation);
                     animationSet_s.addAnimation(rotateAnimation_s);
-                    //animationSet_s.addAnimation(translateAnimation_b);
 
                     bottle.startAnimation(animationSet);
                     shadow.startAnimation(animationSet_s);
 
-                    /*translateAnimation_b.setAnimationListener(new Animation.AnimationListener() {
-                        @Override
-                        public void onAnimationStart(Animation animation) {
-                            //To change body of implemented methods use File | Settings | File Templates.
-                        }
-
-                        @Override
-                        public void onAnimationEnd(Animation animation) {
-                            Log.d("Clownxiaoqiang","in the animation end");
-                            if ((((randomangle/180))%2) != 0){
-                                if(X_b==-0.05f){
-                                    X_b = 0;
-                                    X_e = -0.05f;
-                                    Y_b = 0;
-                                    Y_e = 0.04f;
-                                }else{
-                                    Log.d("Clownxiaoqiang",(randomangle/180)%2+"");
-                                    X_b = -0.05f;
-                                    X_e = 0;
-                                    Y_b = 0.04f;
-                                    Y_e = 0;
-                                }
-                            }
-                        }
-
-                        @Override
-                        public void onAnimationRepeat(Animation animation) {
-                            //To change body of implemented methods use File | Settings | File Templates.
-                        }
-                    });*/
                     rotateAnimation.setAnimationListener(new Animation.AnimationListener() {
                         @Override
                         public void onAnimationStart(Animation animation) {
@@ -171,45 +126,6 @@ public class MyActivity extends Activity {
             }
         });
 
-        //重置按钮
-       /* reset_bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (reset_IsClicking == false){
-                    reset_IsClicking = true;
-                    AnimationSet animationSet = new AnimationSet(true);
-                    RotateAnimation rotateAnimation = new RotateAnimation(angle, 360,
-                            Animation.RELATIVE_TO_SELF, 0.5f,
-                            Animation.RELATIVE_TO_SELF, 0.35f);
-                    //从那个角度开始旋转，旋转多少度，X轴的旋转方式和值，Y轴的旋转方式和值
-                    animationSet.setFillAfter(true);
-                    //保证下一次在旋转前一次位置旋转
-                    rotateAnimation.setDuration(angle+1000);
-                    animationSet.addAnimation(rotateAnimation);
-                    bottle.startAnimation(animationSet);
-                    shadow.startAnimation(animationSet);
-                    rotateAnimation.setAnimationListener(new Animation.AnimationListener() {
-                        @Override
-                        public void onAnimationStart(Animation animation) {
-                            //To change body of implemented methods use File | Settings | File Templates.
-                        }
-
-                        @Override
-                        public void onAnimationEnd(Animation animation) {
-                            angle = 0;
-                            reset_IsClicking = false;
-                            //To change body of implemented methods use File | Settings | File Templates.
-                        }
-
-                        @Override
-                        public void onAnimationRepeat(Animation animation) {
-                            //To change body of implemented methods use File | Settings | File Templates.
-                        }
-                    });
-                }
-            }
-        });*/
-
         //真心话按钮
         realword_bt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -233,15 +149,12 @@ public class MyActivity extends Activity {
 
     }
 
-    //覆写onResume，在调用这个函数时，返回到最开始的状态，这里涉及到了很多Activity状态的函数，
-    //不懂请戳[http://blog.csdn.net/niu_gao/article/details/7103907]
     @Override
     protected void onResume() {
         if(flag_intent == true){
             linearlayout_bottom.setVisibility(View.INVISIBLE);
             relativeLayoutTop.setBackgroundResource(R.drawable.dengguang);
             dialogCreate = new DialogCreate(MyActivity.this,relativeLayoutTop,realword_bt,bigadventure_bt,linearlayout_bottom,bottle);
-            Log.d("Clownxiaoqiang","in onResume");
             flag_intent = false;
             flag_bottom_bt = false;
         }
@@ -253,7 +166,6 @@ public class MyActivity extends Activity {
     @Override
     protected void onPause() {
         dialogCreate.dismiss();
-        Log.d("Clownxiaoqiang", "in onPause");
         MobclickAgent.onPause(this);
         super.onPause();    //To change body of overridden methods use File | Settings | File Templates.
     }
@@ -262,7 +174,6 @@ public class MyActivity extends Activity {
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK){
-            Log.d("Clownxiaoqiang","being Touched");
             if (pic_isRotating == false){
                exit_dialog.ExitDialog();
             }
